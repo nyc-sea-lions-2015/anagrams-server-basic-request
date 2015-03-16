e5c78c536fafde1c4f613ebf7b801cf1579a14cb
Angel Baek & Lauren N. Roth <pair+gellieb.laurennicoleroth@devbootcamp.com>
2015-03-16 15:32:07 -0400

    read each line from the words file and compare it to the anagram word passed in through params

--- source/index.rb
+++ source/index.rb
@@ -5,7 +5,24 @@ get '/' do
 end
 
 get '/anagrams/' do
-  # p params
-  p params[:word]
+  anagram = params[:word]
+
+  words_file = File.open('words.txt', 'r')
+  anagram_array = []
+  words_file.each_line do |line|
+
+    anagram_array << line.chomp if line.chomp.chars.sort.join == anagram.chars.sort.join
+    anagram_array
+  end
+  p anagram_array
+
+
+
+
+  # p "hello".permutation(5).to_a
+  # permutations = anagram.split("").permutation(anagram.length).to_a
+  # # p permutations
+  # permutations.map {|arr| arr.join }
+  # p permutations
 end
 
