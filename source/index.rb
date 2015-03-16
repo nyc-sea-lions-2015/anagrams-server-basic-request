0851f1e9cd529430394a79ade7d3a90917698a50
Kiran & Stephanie Lo <pair+phanendar.steppinlo@devbootcamp.com>
2015-03-16 16:06:11 -0400

    completed challenge

--- source/index.rb
+++ source/index.rb
@@ -2,24 +2,22 @@
 require 'sinatra'
 
 get '/' do
-    'Kiran & Steph are AWESOME!'
+  'Kiran & Steph are AWESOME!'
 end
 
 get '/anagrams/' do
-
-  word = params[:word]
-  word = word.chars.sort!
+  print_word = params[:word]
+  word = print_word.chars.sort!
   final = []
   File.open('words').each_line do |line|
     dic_word = line.chomp.chars.sort
-    p dic_word
-    final << line.chomp if dic_word == word
+    final << line if dic_word == word
   end
-
-  # final.each do |word|
-  #   puts "#{word} "
-  # end
-  p final
-
-
+  if final.empty?
+    return "No Anagrams Found for #{print_word}"
+  else
+    final.each do |word|
+      puts word
+    end
+  end
 end
