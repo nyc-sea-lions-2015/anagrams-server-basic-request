554d82ed4bdb6d46d0c2977cae47e95d8cb18704
Angel Baek & Lauren N. Roth <pair+gellieb.laurennicoleroth@devbootcamp.com>
2015-03-16 16:55:07 -0400

    Make sure word has an anagram, returns correct anagrams that are included in the dictionary only

--- source/index.rb
+++ source/index.rb
@@ -7,16 +7,26 @@ end
 get '/anagrams/' do
   anagram = params[:word]
 
-  words_file = File.open('words.txt', 'r')
   anagram_array = []
-  words_file.each_line do |line|
-    anagram_array << line.chomp if line.chomp.chars.sort.join == anagram.chars.sort.join
-    anagram_array.length == 1 ? "No anagrams found for #{anagram}" : anagram_array
-  end
-
-
   anagrams = []
-  anagram.chars.permutation {|arr| anagrams << arr.join}
-  p anagrams.each {|word| p word.chomp}.uniq.join(", ")
+  File.open('words.txt').each_line do |line|
+    anagram_array << line.chomp if line.chomp.chars.sort == anagram.chars.sort
+    # anagram.chars.permutation {|arr| anagrams << arr.join}.uniq
+    # uniq_anagrams = anagrams.each {|word| word.chomp}.uniq
+  end
+    anagram_array.length == 1 ? "No anagrams found for #{anagram}" : anagram_array.each_slice(3).to_a.join(", ")
+  # p anagram_array
+
+  # anagrams = []
+  # anagram.chars.permutation {|arr| anagrams << arr.join}
+  # # uniq_anagrams = anagrams.each {|word| word.chomp}.uniq
+
+  # p uniq_anagrams
+  # p uniq_anagrams.include?("ant")
+  # words_file.each_line do |word|
+  #   p word.chomp
+  #   # p "included" if uniq_anagrams.include?("ant")
+  # end
+
 end
 
