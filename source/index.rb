d8d7877a846e1383d368e36371785bc864abccd7
Ryan Blecher & Tracy Teague <pair+notryanb.tracyteague@devbootcamp.com>
2015-03-16 17:13:46 -0400

    Complete all releases. Works for words < length of 5. Refactor algorothm

--- source/index.rb
+++ source/index.rb
@@ -14,11 +14,18 @@ end
 
 # Store all matches in a collection and return the response to the user
 # in a comma separated string using cURL
+# error MyCustomError do
+#   'There was no anagram for that word' + env['sinatra.error'].message
+# end
+error 500 do
+  "There are no anagrams for this word"
+end
 
 get '/anagrams' do
     # "#{params[:word]}"
     input_word = params[:word].chars
     word_combos = input_word.permutation(input_word.length)
+
     unique_words = []
     word_combos.each { |word| unique_words << word.join('') }
     unique_words = unique_words.uniq
@@ -28,11 +35,20 @@ get '/anagrams' do
       dictionary << word.chomp
     end
 
+    # small_dict = dictionary.select { |letters| letters =~ /[input_word.join('')]/ }
+
     collection = []
     unique_words.each do |word|
       collection << word if dictionary.include?(word)
     end
 
-    collection.join(',')
-
+    # puts "Collection length:  #{collection.length}"
+    # collection.join(',')
+    if (collection.length <= 1) #|| collection.first == params[:word]
+      return 500
+    else
+      collection.join(',')
+    end
 end
+
+
