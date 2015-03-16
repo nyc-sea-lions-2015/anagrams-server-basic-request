a2b3d96698d551be6d248c7661f11382688794b2
Malcolm Bouzi & Kurt Schnelker <pair+mbouzi.sabron13@devbootcamp.com>
2015-03-16 15:05:27 -0400

    create route to find anagrams

--- source/index.rb
+++ source/index.rb
@@ -4,5 +4,9 @@ get '/' do
     'Anagrams Server'
 end
 
-get '/anagrams' do
+get '/anagrams/:word' do
+    word = params[:word]
+    anagrams = word.split('').permutation.to_a.map{ |word| word.join }
+    p anagrams.join(', ')
+end
 
