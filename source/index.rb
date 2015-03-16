0d8d108a113d6a05b924f67b7f961c5d438c22e6
Dan Asselin & Kevin Alwell <pair+danasselin.kevalwell@devbootcamp.com>
2015-03-16 15:04:00 -0400

    Create rb file to hold dictionary hash with words as keys and values as alphabetically sorted letters of word

--- source/index.rb
+++ source/index.rb
@@ -1,4 +1,21 @@
-get '/' do
+require 'sinatra'
+require_relative 'sortedDictWord'
+
+get '/:anagram' do
   'Anagrams Server'
+
+  File.open('dictWords.txt', 'r').each do |guest_word|
+    params[:anagram].split('').sort.join
+    guest_word.split('').sort.join
+    if params[:anagram] == guest_word
+      anagrams << guest_word
+    end
+    anagrams.each do |anagram|
+      p anagram
+    end
+  end
 end
+
+anagrams = []
+
 
