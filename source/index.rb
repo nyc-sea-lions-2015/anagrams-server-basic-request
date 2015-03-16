79c7dc06e0035a4c4e323f93a54eb3c013f613ab
Malcolm Bouzi & Kurt Schnelker <pair+mbouzi.sabron13@devbootcamp.com>
2015-03-16 16:22:12 -0400

    adds 404 error for no anagrams

--- source/index.rb
+++ source/index.rb
@@ -6,15 +6,22 @@ end
 
 get '/anagrams/:word' do
   word = params[:word]
-  anagrams = word.split('').permutation.to_a.map{ |word| word.join }
-  file = File.open('words', 'r')
   dict = []
+  file = File.open('words', 'r')
   file.each_line do |word|
     dict << word.chomp
   end
+  anagrams = word.split('').permutation.to_a.map{ |word| word.join }
+  words = []
   anagrams.each do |word|
-    p word if dict.include?(word.chomp)
+    words << word if dict.include?(word.chomp)
   end
+  words.delete(word)
+  words.uniq
+  if words.count == 0
+    return [404, "No anagrams found for #{word}"]
+  end
+  p words.join(', ')
 end
 
 
