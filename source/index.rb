4459a06c64a30f11094263df5d05d7a6a9998dd6
Eveanandi Butler & Max Rater <pair+ebutler90.maxrater@devbootcamp.com>
2015-03-16 16:43:32 -0400

    Add server error message

--- source/index.rb
+++ source/index.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 
+
 get '/' do
   'Anagrams Server'
 end
@@ -18,25 +19,18 @@ get '/anagrams' do
 
   word_container = []
   word_array.permutation(word_array.length).to_a.each do |word|
-   anagram = word.join("")
-  # word_container << anagram
-  # end
+    anagram = word.join("")
     if dictionary.include?(anagram)
     word_container  << anagram
     end
-
   end
   word_container.uniq.join(",")
 end
 
+error 500 do
+  'There are no anagrams for this word.'
+end
 
 
-# def run (input)
-#   input * 2
-# end
 
-# @word is going to be a string
-# word_array = @word.chars
-# word_array.permutation(word_array.length).to_a
-#join each word in word_arr
 
