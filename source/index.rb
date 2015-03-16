e9a78fa72eddf71ff8b7bb925e3146f5c011de32
Malcolm Bouzi & Kurt Schnelker <pair+mbouzi.sabron13@devbootcamp.com>
2015-03-16 15:45:34 -0400

    finds all words in dictionary

--- source/index.rb
+++ source/index.rb
@@ -5,8 +5,16 @@ get '/' do
 end
 
 get '/anagrams/:word' do
-    word = params[:word]
-    anagrams = word.split('').permutation.to_a.map{ |word| word.join }
-    p anagrams.join(', ')
+  word = params[:word]
+  anagrams = word.split('').permutation.to_a.map{ |word| word.join }
+  file = File.open('words', 'r')
+  dict = []
+  file.each_line do |word|
+    dict << word.chomp
+  end
+  anagrams.each do |word|
+    p word if dict.include?(word.chomp)
+  end
 end
+
 
