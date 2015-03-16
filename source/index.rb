cddab071a4521ea520e243df39691f076ecf5834
Ryan & Awesome Ninja Fighter <pair+rimmesbe.samguergen@devbootcamp.com>
2015-03-16 14:56:52 -0400

    http verb can search through words file and find match

--- source/index.rb
+++ source/index.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
+# require_relative 'words'
 
 get '/' do
 
   'Anagrams Server'
+
+end
+
+
+get '/anarams' do
+value = params[:word]
+
+
+  puts params
+  puts "ok" if File.read("words").include?(value)
 
 end
